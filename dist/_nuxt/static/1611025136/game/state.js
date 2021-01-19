32763e7f07527a346831c5d49b916951b8c61629
Jim Strother <jim.strother@gmail.com>
2021-01-18 21:01:22 -0600

    gh-pages first commit!

---
+++ dist/_nuxt/static/1611025136/game/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611025136",layout:"default",error:null,state:{selectedQuote:a,selectedMovie:a,encryptedQuote:a,encryptedLetterIndexes:{},userAnswer:[],isAnswerCorrect:false},serverRendered:true,routePath:"\u002Fgame",config:{content:{dbHash:"b25b294c"}}}}(""));
