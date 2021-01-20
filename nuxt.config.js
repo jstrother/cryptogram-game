b782164537c900e9e683ded6f609675d431cbd2b
Jim Strother <jim.strother@gmail.com>
2021-01-19 22:04:01 -0600

    Modified nuxt.config.js added color codes directly rather than import them due to Error: Cannot find module 'vuetify/es5/util/colors'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors';
-
 export default {
   target: 'server',
   head: {
@@ -27,13 +25,13 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+          primary: '#1976D2',
+          accent: '#424242',
+          secondary: '#FF8F00',
+          info: '#26A69A',
+          warning: '#FFC107',
+          error: '#DD2C00',
+          success: '#00E676',
         },
       },
     },
